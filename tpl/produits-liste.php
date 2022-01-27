df47499bbfc913686469245805d4afebb9123418
Mathieu Vigou <mathieu.vigou@gmail.com>
2022-01-27 13:48:09 +0100

    Design liste produits

--- tpl/produits-liste.php
+++ tpl/produits-liste.php
@@ -1,11 +1,35 @@
 <?php if(!$GLOBALS['domain']) exit; ?>
 
-<section id="basket-list" class="mw960p center">
+<section>
 
-	<?
-		$_GET['mode']='liste-produits';
-		include('theme/'.$GLOBALS['theme'].'/panier.ajax.php');
-	?>
+	<?h1('titre')?>
+
+	<table class="liste-produits w100 tl">
+
+		<thead>
+
+            <tr class="liste-produits--entete sm:hidden md:tr">
+                <th class="up normal pys pxs">Produit</th>
+                <th class="up normal pys pxs">Description</th>
+                <th class="up normal pys pxs">Prix unitaire</th>
+                <th class="up normal pys pxs w250p" title="Boutons d'action" aria-label="Boutons d'action"></th>
+            </tr>
+
+			<tr class="liste-produits--entete sm:tr md:hidden">
+                <th class="normal pys" colspan="3">Catalogue</th>
+            </tr>
+
+        </thead>
+
+		<tbody>
+
+			<?
+				$_GET['mode']='liste-produits';
+				include('theme/'.$GLOBALS['theme'].'/panier.ajax.php');
+			?>
+
+		</tbody>
+	</table>
 
 </section>
 
